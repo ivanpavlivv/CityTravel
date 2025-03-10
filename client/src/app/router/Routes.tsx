16fd75e7dc327190968aeae1e1fe9cf2a63b954c
ivanpavlivv <91207026+ivanpavlivv@users.noreply.github.com>
2025-03-10 16:58:27 +0200

    Added client-side error handling

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,9 +1,12 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import CityDashboard from "../../features/cities/dashboard/CityDashboard";
 import CityForm from "../../features/cities/form/CityForm";
 import CityDetails from "../../features/cities/details/CityDetails";
+import NotFound from "../../features/errors/NotFound";
+import TestErrors from "../../features/errors/TestErrors";
+import ServerError from "../../features/errors/ServerError";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +18,10 @@ export const router = createBrowserRouter([
       { path: "cities/:id", element: <CityDetails /> },
       { path: "createCity", element: <CityForm key="create" /> },
       { path: "manage/:id", element: <CityForm /> },
+      { path: "not-found", element: <NotFound /> },
+      { path: "server-error", element: <ServerError /> },
+      { path: "errors", element: <TestErrors /> },
+      { path: "*", element: <Navigate replace to="/not-found" /> },
     ],
   },
 ]);
